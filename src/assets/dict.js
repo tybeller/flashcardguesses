6a0170bac36accef0f79405c5d8f0009b83b655d
tybeller <ty.c.beller@gmail.com>
2023-09-23 18:12:42 -0400

    added dictionary

---
+++ src/assets/dict.js
@@ -0,0 +1,52 @@
+let latinDictionary = [
+    {
+        "word": "bardus",
+        "definition": "slow, dull"
+    },
+    {
+        "word": "brevis",
+        "definition": "short, brief"
+    },
+    {
+        "word": "comminus",
+        "definition": "in close combat"
+    },
+    {
+        "word": "electus",
+        "definition": "chosen"
+    },
+    {
+        "word": "extremus",
+        "definition": "last, extreme"
+    },
+    {
+        "word": "gravatus",
+        "definition": "sick, ill"
+    },
+    {
+        "word": "gravis",
+        "definition": "heavy, serious, important, grievous"
+    }
+    {
+        "word": "hodiernus",
+        "definition": "of today"
+    },
+    {
+        "word": "honorabilis",
+        "definition": "respectful"
+    },
+    {
+        "word": "idoneus",
+        "definition": "proper, fitting, capable"
+    },
+    {
+        "word": "ignarus",
+        "definition": "ignorant"
+    },
+    {
+        "word": "ignavus",
+        "definition": "lazy, inert, sluggish, a coward"
+    },
+]
+
+export default latinDictionary;
